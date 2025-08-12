01c4ad14ea3747d0ac8c6ba382af98cadacc5a61
clover <76261419+goatgirlclover@users.noreply.github.com>
2025-08-11 23:24:12 -0700

    add generic build filenames

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig, passthroughImageService } from "astro/config";
 
+import genericBuildFilenames from "astro-generic-build-filenames";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
     service: passthroughImageService(),
   },
+
+  integrations: [genericBuildFilenames()],
 });
