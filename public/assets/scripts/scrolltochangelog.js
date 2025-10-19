c401fc3fbc3ae0cf99a9b7fb7be325b7d1acaf28
clover <76261419+goatgirlclover@users.noreply.github.com>
2025-10-18 20:12:12 -0700

    add scroll to changelog

---
+++ public/assets/scripts/scrolltochangelog.js
@@ -0,0 +1,15 @@
+function scrollToChangelogItem() {
+  const changelogDate = new URLSearchParams(window.location.search).get("changelog-item").replace("/", "");
+  if (!changelogDate) return;
+
+  const changelogItems = document.querySelectorAll("#site-changelog li");
+  for (const item of changelogItems) {
+    if (item.textContent.trim().startsWith(changelogDate)) {
+        item.className += " highlight-animation"
+        item.scrollIntoView();
+        break;
+    }
+  }
+}
+
+window.addEventListener("load", scrollToChangelogItem);
