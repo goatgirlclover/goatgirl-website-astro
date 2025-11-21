cd49237c3cf119016363cb6c7a033063c547af98
clover <76261419+goatgirlclover@users.noreply.github.com>
2025-11-21 15:59:22 -0700

    migrate gallery to ContentCollections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
+import fs from "fs";
 
 const blog = defineCollection({
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
@@ -12,4 +13,56 @@ const blog = defineCollection({
     }),
 });
 
-export const collections = { blog };
+export type GalleryEntry = z.infer<typeof GallerySchema>;
+const GallerySchema = z.object({
+	id: z.string(),
+	imagePath: z.string(),
+	thumbnailPath: z.string(),
+	path: z.string(),
+	year: z.number(),
+
+	title: z.string(),
+	description: z.string(),
+	altText: z.string(),
+	rssDate: z.string(),
+
+	imageWidth: z.number(),
+	imageHeight: z.number(),
+});
+
+const gallery = defineCollection({
+	loader: async() => {
+		var galleryEntries: GalleryEntry[] = [];
+		fs.readdirSync("./src/content/gallery/").forEach(async (file: string) => {
+			if (file.endsWith(".txt")) {
+				const entryID = file.slice(0, -4);
+				const fullFilePath = "./src/content/gallery/" + file;
+				const info = fs.readFileSync(fullFilePath, 'utf-8').split(/\r?\n/);
+				const resolution = info[4].split('x');
+
+				const parsedEntry:GalleryEntry = ({
+					id: entryID,
+					imagePath: "/gallery/img/" + entryID + ".png",
+					thumbnailPath: "/gallery/thumb/" + entryID + ".jpg",
+					path: "/gallery/view/" + entryID + "/",
+					year: Number(entryID.substring(0, 4)),
+
+					title: info[0].trim(),
+					description: info[1].trim(),
+					rssDate: info[2].trim(),
+					altText: info[3].trim(),
+
+					imageWidth: Number(resolution[0].trim()),
+					imageHeight: Number(resolution[1].trim()),
+				})
+
+				galleryEntries.push(parsedEntry);
+			}
+		});
+
+		return galleryEntries;
+    },
+	schema: GallerySchema,
+}); 
+
+export const collections = { blog, gallery };
